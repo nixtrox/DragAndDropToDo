74833bc100374aaed3dcb45be9bab3f96c5e8243
nixtrox <nixtrox.vidi@gmail.com>
2024-04-14 19:36:07 +0200

    ColContainer

---
+++ dragAndDrop/src/components/ColumnContainer.tsx
@@ -0,0 +1,16 @@
+import { Column } from "../types";
+
+interface Props{
+    column: Column;
+}
+
+
+
+
+function ColumnContainer(props: Props)
+{
+   const {column} = props;
+    return <div>{column.title}</div>
+}
+
+export default ColumnContainer;
--- dragAndDrop/src/components/KanbanBoard.tsx
+++ dragAndDrop/src/components/KanbanBoard.tsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
 import { Column } from "../types";
+import ColumnContainer from "./ColumnContainer";
 function KanbanBoard(){
 
     const [columns, setColumns] = useState<Column[]>([]);
-
+    console.log(columns)
 
 
 
     return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]"  >
-        <div className="m-auto">
-            <button className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-z border-colimnBackgroundColor p-4 ring-rose-500 hover:ring-2 ">Add Column</button>
+        <div className="m-auto flex gap-4">
+            <div className="flex gap-4" >
+                {columns.map((col) =>(
+                <ColumnContainer column={col}  ></ColumnContainer>
+            ))}
+            </div>
+            <button onClick={() => {
+                createNewColumn();
+            }}    
+            className="h-[60px] 
+            w-[350px] 
+            min-w-[350px] 
+            cursor-pointer 
+            rounded-lg 
+            bg-mainBackgroundColor 
+            border-z 
+            border-colimnBackgroundColor 
+            p-4 
+            ring-rose-500 
+            hover:ring-2 ">
+                Add Column</button>
         </div>
         
     </div>
