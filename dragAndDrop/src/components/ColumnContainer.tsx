3518873431d247f21f78f376ed8e56a0cb7fe35c
nixtrox <nixtrox.vidi@gmail.com>
2024-04-14 19:46:35 +0200

    Column rendezes

--- dragAndDrop/src/components/ColumnContainer.tsx
+++ dragAndDrop/src/components/ColumnContainer.tsx
@@ -10,7 +10,22 @@ interface Props{
 function ColumnContainer(props: Props)
 {
    const {column} = props;
-    return <div>{column.title}</div>
+    return <div className="
+    bg-mainBackgroundColor 
+    color-white
+    w-[350px]
+    h-[500px]
+    max-h-[500px]
+    rounded-md
+    flex
+    flex-col
+    text-white" 
+  
+    >
+        <div> {column.title}</div>
+        <div className="flex flex-grow"  >asd</div>
+        <div>Footer</div>
+        </div>
 }
 
 export default ColumnContainer;
--- dragAndDrop/src/components/KanbanBoard.tsx
+++ dragAndDrop/src/components/KanbanBoard.tsx
@@ -9,7 +9,15 @@ function KanbanBoard(){
 
 
     return (
-    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]"  >
+    <div className="
+    m-auto 
+    flex 
+    min-h-screen 
+    w-full 
+    items-center 
+    overflow-x-auto 
+    overflow-y-hidden 
+    px-[40px]"  >
         <div className="m-auto flex gap-4">
             <div className="flex gap-4" >
                 {columns.map((col) =>(
