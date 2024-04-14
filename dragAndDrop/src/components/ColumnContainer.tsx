422570ff512d64c879d7a6bc01163abdda79258f
nixtrox <nixtrox.vidi@gmail.com>
2024-04-14 19:57:21 +0200

    basic Style

--- dragAndDrop/src/components/ColumnContainer.tsx
+++ dragAndDrop/src/components/ColumnContainer.tsx
@@ -22,7 +22,43 @@ function ColumnContainer(props: Props)
     text-white" 
   
     >
-        <div> {column.title}</div>
+        
+        <div className="
+        bg-mainBackgroundColor
+        text-md
+        h-[60px]
+        cursor-grab
+        rounded-md
+        rounded-b-none
+        p-3
+        font-bold
+        border-columnBackgroundColor
+        border-4
+        flex
+        items-center
+        justify-between"  > 
+
+         <div className="flex gap-2">
+         <div className="
+         flex
+         justify-center
+         items-center
+         bg-columnBackgroundColor
+         px-2
+         py-1
+         text-sm
+         rounded-full" >
+            0
+            </div>
+         {column.title}
+         </div>
+         </div>
+
+         
+      
+
+
+
         <div className="flex flex-grow"  >asd</div>
         <div>Footer</div>
         </div>
